890f2c21a6e254c0aa8f34cb47092ddad4d2892c
Leonam Pereira Dias <leonam.pd@gmail.com>
2019-05-14 07:45:22 -0300

    insert template on checkout

--- fechar_pedido.php
+++ fechar_pedido.php
@@ -10,4 +10,16 @@ $livroFisico = new LivroFisico(12.90, 'PHP OO', 'Livro mto bom', 300);
 
 $itemCarrinho = new ItemCarrinho(10, $ebook);
 
-echo $itemCarrinho->subtotal();
+?>
+
+<?php include __DIR__ . '/templates/header.php'; ?>
+<body>
+    <?php include __DIR__ . '/templates/menu.php'; ?>
+    <div class="container">
+        <div class="alert alert-warning" role="alert">
+            <h1>Obrigado!</h1>
+
+            <p>Seu pedido <strong>123456</strong> foi registrado no nosso sistema e em breve você receberá um e-mail com maiores detalhes.</p>
+        </div>
+    </div>
+<?php include __DIR__ . '/templates/footer.php'; ?>

4010e1c483abe67c96eee67b81e09514327a700b
Leonam Pereira Dias <leonam.pd@gmail.com>
2019-12-05 07:08:49 -0300

    projeto: ajusta o projeto para que ele possa servir de repo template (#2)
    * create basic classes for the project
    
    * rename file
    
    * create reused template files for header, menu and footer
    
    * create checkout page
    
    * insert template on checkout
    
    * add link to homepage
    
    * create docker environment
    
    * home: altera dados dos produtos
    
    Insere livros que são interessantes comprar e ler.
    
    * checkout: insere formulario para envio dos dados pro fechamento do
    pedido
    
    * projeto: remove trechos que enviesam a solução
    
    Dado que são arquivos que implementam parte da solução, faz sentido
    removê-los para n enviesar a solução do projeto

--- carrinho.php
+++ carrinho.php
@@ -1,23 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/ecommerce.css">
-    <title>Meu E-commerce Bacanudo</title>
-</head>
+<?php include __DIR__ . '/templates/header.php'; ?>
 <body>
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-        <h5 class="my-0 mr-md-auto font-weight-normal">Meu ecommerce bacanudo</h5>
-        <nav class="my-2 my-md-0 mr-md-3">
-            <a class="p-2 text-dark" href="#">Lorem</a>
-            <a class="p-2 text-dark" href="#">Ipsum</a>
-            <a class="p-2 text-dark" href="#">Dolor</a>
-            <a id="place-order" class="p-2 text-dark" href="#">Ver carrinho</a>
-        </nav>
-    </div>
+    <?php include __DIR__ . '/templates/menu.php'; ?>
     <div class="container">
         <h2>Carrinho</h2>
 
@@ -51,9 +34,4 @@
             </div>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script src="js/app.js"></script>
-</body>
-</html>
+<?php include __DIR__ . '/templates/footer.php';

e61dd3c75e6bfdff172d43cf240cca7bc68feb9a
Leonam Pereira Dias <leonam.pd@gmail.com>
2019-12-05 07:01:03 -0300

    projeto: remove trechos que enviesam a solução
    Dado que são arquivos que implementam parte da solução, faz sentido
    removê-los para n enviesar a solução do projeto

--- fechar_pedido.php
+++ fechar_pedido.php
@@ -1,17 +1,3 @@
-<?php
-
-include 'classes/Produto.php';
-include 'classes/Ebook.php';
-include 'classes/LivroFisico.php';
-include 'classes/ItemCarrinho.php';
-
-$ebook = new Ebook(10.90, 'PHP OO', 'Livro mto bom', 'http://xxx');
-$livroFisico = new LivroFisico(12.90, 'PHP OO', 'Livro mto bom', 300);
-
-$itemCarrinho = new ItemCarrinho(10, $ebook);
-
-?>
-
 <?php include __DIR__ . '/templates/header.php'; ?>
 <body>
     <?php include __DIR__ . '/templates/menu.php'; ?>
